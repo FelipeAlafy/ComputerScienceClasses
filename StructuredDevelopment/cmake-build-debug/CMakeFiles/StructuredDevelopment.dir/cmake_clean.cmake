e959efcba05e003d9106253d8832ffb2472caa00
Felipe Alafy <git@felipealafy.net>
2025-04-24 19:51:34 -0300

    added .gitignore

---
+++ StructuredDevelopment/cmake-build-debug/CMakeFiles/StructuredDevelopment.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/StructuredDevelopment.dir/Class_1/main.c.o"
+  "CMakeFiles/StructuredDevelopment.dir/Class_1/main.c.o.d"
+  "StructuredDevelopment"
+  "StructuredDevelopment.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/StructuredDevelopment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ StructuredDevelopment/cmake-build-debug/CMakeFiles/StructuredDevelopment.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for StructuredDevelopment.
+# This may be replaced when dependencies are built.
